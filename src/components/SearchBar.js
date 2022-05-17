0784d1e550e3cd4f7d979df5108ec285845176ff
Racso1624 <fer10_lb@outlook.com>
2022-05-17 15:08:58 -0600

    Se agregan elementos a la search bar

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,10 +1,13 @@
 import React from "react";
 import Logo from "../images/youtube_logo.png"
+import Menu from "../images/menu.png"
 import "../styles/searchbar.sass";
 
 const SearchBar = () =>(
     <div className="searchbar">
-        <img src={Logo} />
+        <img className="menu" src={Menu} />
+        <img className="logo" src={Logo} />
+        <div className="search"></div>
     </div>
 )
 
