19b01eb7ae2d444e99565860ce8fee29d84e3c5b
Racso1624 <fer10_lb@outlook.com>
2022-05-21 16:56:24 -0600

    Se agregan iconos

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,13 +1,19 @@
 import React from "react";
 import Logo from "../images/youtube_logo.png"
 import Menu from "../images/menu.png"
+import SearchIcon from "../images/search_icon.png"
 import "../styles/searchbar.sass";
 
 const SearchBar = () =>(
     <div className="searchbar">
         <img className="menu" src={Menu} />
         <img className="logo" src={Logo} />
-        <div className="search"></div>
+        <div className="searchbox">
+            <div className="box"></div>
+            <div className="searchbutton">
+                <img className="searchicon" src={SearchIcon} />
+            </div>
+        </div>
     </div>
 )
 
