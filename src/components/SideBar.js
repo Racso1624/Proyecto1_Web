b87d9b5b68acba6b31ed91eefe147889da2932be
Racso1624 <fer10_lb@outlook.com>
2022-05-26 11:53:12 -0600

    Se agregan secciones en SideBar

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -2,18 +2,21 @@ import React from "react";
 import { HomeIcon, ExploreIcon, ShortsIcon, SubscriptionsIcon } from "./SideBar_Icons";
 import "../styles/sidebar.sass"
 
-var lista_iconos = [HomeIcon, ExploreIcon, ShortsIcon, SubscriptionsIcon]
-var lista_seccion = ['Inicio', 'Explorar', 'Shorts', 'Subscripciones']
+const lista_iconos_1 = [<HomeIcon/>, <ExploreIcon/>, <ShortsIcon/>, <SubscriptionsIcon/>]
+const lista_seccion_1 = ['Inicio', 'Explorar', 'Shorts', 'Subscripciones']
 
 const SideBar = () =>(
     <div className="sidebar">
+        <div className="seccion-1">
         {
-            lista_iconos.map((elemento, posicion) => {
+            lista_iconos_1.map((elemento, posicion) => {
                 return <div className="seccion-sidebar" key={posicion}>
-                    <elemento/>
+                    {elemento}
+                    {lista_seccion_1.shift()}
                 </div>
             })
         }
+        </div>
     </div>
 )
 
