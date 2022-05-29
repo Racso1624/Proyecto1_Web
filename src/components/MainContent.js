56f51f1eba03e10342fb0c6f800f6f01c50feaa1
Racso1624 <fer10_lb@outlook.com>
2022-05-29 00:19:49 -0600

    Se agregan configs a Main Content

--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -25,7 +25,10 @@ const MainContent = () =>(
     <div className="main-content">
         <Content link="https://i.ytimg.com/vi/VaF92M2dKmQ/hqdefault.jpg?sqp=-oaymwEcCOADEI4CSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBGDyQ6vahINDWsGF5mb2fKLoB21w"
         profile="https://yt3.ggpht.com/ytc/AKedOLRG_0HFQR5XZ0rWQTvmfoYpM2zLLZjePQkZ6EoF=s68-c-k-c0x00ffffff-no-rj"
-        title="Harry Styles - As It Was in the Live Lounge"/>
+        title="Harry Styles - As It Was in the Live Lounge"
+        channel="BBC"
+        views="1M"
+        hours="13"/>
     </div>
 
 )
